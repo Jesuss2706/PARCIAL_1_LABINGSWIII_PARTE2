93c038bb946a6fa1ad14ae2ce634d89941454e08
Jesuss2706 <jeslamu2006@gmail.com>
2026-09-23 22:38:15 -0500

    si

--- Parcial1/src/app/medicos/medicos.ts
+++ Parcial1/src/app/medicos/medicos.ts
@@ -60,14 +60,14 @@ export class Medicos {
   protected readonly especialidadSeleccionada = signal<string>('terapiaNeural');
 
   protected readonly medicos: Medico[] = [
-    { nombres: 'Juan', apellidos: 'Pérez', especialidad: 'Fisioterapia Deportiva', frase: 'Comprometido con tu recuperación.', imagen: 'https://picsum.photos/id/64/300/220' },
-    { nombres: 'Catalina', apellidos: 'Sánchez', especialidad: 'Quiropraxia', frase: 'La salud es fundamental.', imagen: 'https://picsum.photos/id/65/300/220' },
-    { nombres: 'Andrés', apellidos: 'Cardozo', especialidad: 'Nutrición y Dietética Terapéutica', frase: 'Un alimento sano alarga la vida.', imagen: 'https://picsum.photos/id/91/300/220' },
-    { nombres: 'Laura', apellidos: 'Gómez', especialidad: 'Terapia Neural', frase: 'Cada avance cuenta en tu bienestar.', imagen: 'https://picsum.photos/id/177/300/220' },
-    { nombres: 'Carlos', apellidos: 'Ramírez', especialidad: 'Cardiología', frase: 'Cuidamos tu corazón en cada consulta.', imagen: 'https://picsum.photos/id/203/300/220' },
-    { nombres: 'Valentina', apellidos: 'Ortiz', especialidad: 'Pediatría', frase: 'Creciendo sanos, paso a paso.', imagen: 'https://picsum.photos/id/338/300/220' },
-    { nombres: 'Miguel', apellidos: 'Torres', especialidad: 'Fisioterapia', frase: 'Movimiento sin dolor, esa es la meta.', imagen: 'https://picsum.photos/id/342/300/220' },
-    { nombres: 'Sofía', apellidos: 'Londoño', especialidad: 'Quiropraxia', frase: 'Alineando tu bienestar desde la raíz.', imagen: 'https://picsum.photos/id/365/300/220' },
+    { nombres: 'Juan', apellidos: 'Pérez', especialidad: 'Fisioterapia Deportiva', frase: 'Comprometido con tu recuperación.', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUV34qHI1VzjhQ95-jcbLTDLf4cnhxbCZXKyym9VABs6qUQLKocWWUDLI&s=10' },
+    { nombres: 'Catalina', apellidos: 'Sánchez', especialidad: 'Quiropraxia', frase: 'La salud es fundamental.', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBKVt58disBcktgc6oa2MXIXgumHidJ_NkLfdFZbOPCiRxUOW950CjGpo&s=10' },
+    { nombres: 'Andrés', apellidos: 'Cardozo', especialidad: 'Nutrición y Dietética Terapéutica', frase: 'Un alimento sano alarga la vida.', imagen: 'https://www.semana.com/resizer/v2/DS4BQDIPCBDIXGC2TI3IATVDRQ.PNG?auth=bb009a1b189e7e241c0d9bf08c9a027adca65322b7cab802ef0fabf9a0344603&smart=true&quality=75&width=1920' },
+    { nombres: 'Laura', apellidos: 'Gómez', especialidad: 'Terapia Neural', frase: 'Cada avance cuenta en tu bienestar.', imagen: 'https://hips.hearstapps.com/hmg-prod/images/f-elconfidencial-com-original-e8d-cdd-e1a-e8dcdde1ad0b59abf5397921858235dd-1640206393.jpg?crop=0.564xw:1.00xh;0.299xw,0&resize=1200:*' },
+    { nombres: 'Carlos', apellidos: 'Ramírez', especialidad: 'Cardiología', frase: 'Cuidamos tu corazón en cada consulta.', imagen: 'https://static.wikia.nocookie.net/dr-house/images/b/bc/House.jpg/revision/latest?cb=20090927231310&path-prefix=es' },
+    { nombres: 'Valentina', apellidos: 'Ortiz', especialidad: 'Pediatría', frase: 'Creciendo sanos, paso a paso.', imagen: 'https://static.guiainfantil.com/media/51260/c/que-debemos-considerar-a-la-hora-de-elegir-pediatra-lg.jpg' },
+    { nombres: 'Miguel', apellidos: 'Torres', especialidad: 'Fisioterapia', frase: 'Movimiento sin dolor, esa es la meta.', imagen: 'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/series-television/the-good-doctor-serie-telecinco-actor-freddie-highmore/138140869-1-esl-ES/Por-que-debes-ver-The-Good-Doctor-en-Telecinco.jpg?crop=0.666015625xw:1xh;center,top&resize=1200:*' },
+    { nombres: 'Sofía', apellidos: 'Londoño', especialidad: 'Quiropraxia', frase: 'Alineando tu bienestar desde la raíz.', imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiTYPx0trxnw5viyxi0s3woNS4S1SUAWpD-_HvcU9kqvOunH82jlGmu3kq&s=10' },
   ];
 
   protected seleccionarEspecialidad(clave: string): void {
